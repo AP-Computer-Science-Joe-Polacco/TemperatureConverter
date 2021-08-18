c2f273682ddd987849cf506f12f7d9e5c7170cf7
jpolacco <jpolacco@gmail.com>
2021-08-18 11:28:56 -0700

    Update TemperatureConverterTest.java

--- TemperatureConverterTest.java
+++ TemperatureConverterTest.java
@@ -12,23 +12,31 @@ import org.junit.Test;
  */
 public class TemperatureConverterTest
 {
-    
-    @Test
-    public void testFToC() {
-        TemperatureConverter converter = new TemperatureConverter();
-
-        // assert statements
-        assertEquals(100.0, converter.fToC(212.0), 0.0000001);
-        assertEquals(-40.0, converter.fToC(-40.0), 0.0000001);
-        assertEquals(0.0, converter.fToC(32.0), 0.0000001);
-        assertEquals(232.77777777, converter.fToC(451.0), 0.0000001);
-        
-       
-    }
   
     
+    @Test
+    public void testFToCPositive() {
+        TemperatureConverter converter = new TemperatureConverter();
+        // assert statements
+        assertEquals(100.0, converter.fToC(212.0), 0.0000001);
+        assertEquals(232.77777777, converter.fToC(451.0), 0.0000001);        
+    }
     
-    
+    @Test
+    public void testFToCNegative() {
+        TemperatureConverter converter = new TemperatureConverter();
+        // assert statements
+        assertEquals(-40.0, converter.fToC(-40.0), 0.0000001); 
+    }
+        
+    @Test
+    public void testFToCZero() {
+        TemperatureConverter converter = new TemperatureConverter();
+        // assert statements
+        assertEquals(0.0, converter.fToC(32.0), 0.0000001);
+    }
+  
+ 
     
     /**
      * Default constructor for test class TemperatureConverterTest
